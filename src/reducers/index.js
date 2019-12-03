e3088950aff2d38ac5ebc2c03139a98c4c68b486
Ahmet Şimşek <ahmet.simsek@getir.com>
2019-12-03 09:18:11 +0300

    redux-immer added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,49 +1,58 @@
-import { combineReducers } from "redux";
-import { createReducer, createActions } from "reduxsauce";
+import {createReducer, createActions} from 'reduxsauce';
+import {combineReducers} from 'redux-immer';
+import produce from 'immer';
 
 // types and actions
-export const { Types, Creators } = createActions(
+export const {Types, Creators} = createActions(
   {
     ping: null,
     pong: null,
     toggleStatus: null,
-    changeDelay: ["delayTime"]
+    changeDelay: ['delayTime'],
   },
-  {}
+  {},
 );
 
 // initial state
 export const INITIAL_STATE = {
   stage: null,
-  delayTime: 500
+  delayTime: 500,
 };
 
 // reducers
 export const toggleStatus = state => {
-  return { ...state, stage: state.stage ? null : "ping" };
+  state.stage = state.stage ? null : 'ping';
+
+  return state;
 };
 
 export const ping = state => {
-  return { ...state, stage: "ping" };
+  state.stage = 'ping';
+
+  return state;
 };
 
 export const pong = state => {
-  return { ...state, stage: "pong" };
+  state.stage = 'pong';
+
+  return state;
 };
 
-export const changeDelay = (state, { delayTime }) => {
-  return { ...state, delayTime };
+export const changeDelay = (state, {delayTime}) => {
+  state.delayTime = delayTime;
+
+  return state;
 };
 
 export const reducer = createReducer(INITIAL_STATE, {
   [Types.TOGGLE_STATUS]: toggleStatus,
   [Types.PING]: ping,
   [Types.PONG]: pong,
-  [Types.CHANGE_DELAY]: changeDelay
+  [Types.CHANGE_DELAY]: changeDelay,
 });
 
-const rootReducer = combineReducers({
-  app: reducer
+const rootReducer = combineReducers(produce, {
+  app: reducer,
 });
 
 export default rootReducer;
